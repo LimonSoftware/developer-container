58632025d2dc09cd71ad152d90684f88dda6fd31
Anibal Limon <anibal@limonsoftware.com>
2025-06-05 08:43:55 -0600

    services: add WORKSPACE_RUN_DIR and use it on tailscaled
    Signed-off-by: Anibal Limon <anibal@limonsoftware.com>

--- image/env-devel-environ.sh
+++ image/env-devel-environ.sh
@@ -5,9 +5,15 @@
 # STORAGE_DIR: User sotrage directory inside the container, useful
 #              to share storage across Host and Container.
 #
+# WORKSPACE_RUN_DIR: Run directory variable to persist across container
+#                    runs.
+#
 
 # Container mapped workspace
 WORKSPACE_DIR="/workspace"
 export WORKSPACE_DIR
 STORAGE_DIR="/storage"
 export STORAGE_DIR
+
+WORKSPACE_RUN_DIR="$WORKSPACE_DIR/.run"
+export WORKSPACE_RUN_DIR
--- image/services/tailscaled.sh
+++ image/services/tailscaled.sh
@@ -5,7 +5,7 @@
  
 source /etc/default/tailscaled
 
-TAILSCALED_DIR="$WORKSPACE_DIR/run/tailscaled"
+TAILSCALED_DIR="$WORKSPACE_RUN_DIR/tailscaled"
 mkdir -p "$TAILSCALED_DIR"
 
 /usr/sbin/tailscaled --state="$TAILSCALED_DIR/tailscaled.state" --socket=/run/tailscale/tailscaled.sock --port=${PORT} $FLAGS
