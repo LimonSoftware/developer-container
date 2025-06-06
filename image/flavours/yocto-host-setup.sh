b007d326cab00cfd42f90217fd6d2330182ed2c5
Anibal Limon <anibal@limonsoftware.com>
2025-06-06 12:52:03 -0600

    flavours: yocto support to create kvm group and add to the user
    The yocto-host-setup.sh is responsiblie to expand yocto-host.sh.in
    at build time, move all KVM and Loop variables related to the
    yocto-host.sh.in template.
    
    Remove validation of Kvm and Loop devices at yocto-run.sh already done
    in this new yocto-host-setup.sh step.
    
    Signed-off-by: Anibal Limon <anibal@limonsoftware.com>

---
+++ image/flavours/yocto-host-setup.sh
@@ -0,0 +1,25 @@
+#
+# Yocto host setup support
+#
+
+HOST_DEVICE_KVM="${HOST_DEVICE_KVN:-/dev/kvm}"
+HOST_DEVICE_KVM_GID="0"
+HOST_DEVICE_LOOP="${HOST_DEVICE_LOOP:-/dev/loop-control}"
+
+if [ -c "${HOST_DEVICE_KVM}" ]; then
+	HOST_DEVICE_KVM_GID="$(stat -c %g "$HOST_DEVICE_KVM")"
+else
+	echo "Warning: KVM device $(HOST_DEVICE_KVM) not found, accel will not work."
+	HOST_DEVICE_KVM=""
+fi
+
+if [ ! -c "${HOST_DEVICE_LOOP}" ]; then
+	echo "Warning: Loop device $(HOST_DEVICE_LOOP) not found."
+	HOST_DEVICE_LOOP=""
+fi
+
+yocto_host="flavours/yocto-host.sh"
+cp "$yocto_host.in" "$yocto_host"
+sed -i "s|__HOST_DEVICE_KVM__|$HOST_DEVICE_KVM|g" "$yocto_host"
+sed -i "s|__HOST_DEVICE_KVM_GID__|$HOST_DEVICE_KVM_GID|g" "$yocto_host"
+sed -i "s|__HOST_DEVICE_LOOP__|$HOST_DEVICE_LOOP|g" "$yocto_host"
---
+++ image/flavours/yocto-setup.sh
@@ -0,0 +1,5 @@
+#
+# Yocto setup customizations
+#
+
+groupadd -g $HOST_DEVICE_KVM_GID "$HOST_DEVICE_KVM_GNAME"
