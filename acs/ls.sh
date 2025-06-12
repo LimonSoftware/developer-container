57f43632c208ae14842cfed45e4de47c69925fb6
Anibal Limon <anibal@limonsoftware.com>
2025-06-12 10:36:51 -0600

    acs: Add support of ls module get access details
    We support very simple cmdline via ssh in order to get IPv4 of
    devices.
    
    Signed-off-by: Anibal Limon <anibal@limonsoftware.com>

---
+++ acs/ls.sh
@@ -0,0 +1,36 @@
+#
+# ACS LimonSoftware devices
+#
+# Environment
+#
+# DC_ACS_LS_SERVER_HOST: Server hostname/ip to get IP based on ID, <required>.
+# DC_ACS_LS_SERVER_USER: User used to get IP based on IDs. default: 'device' (optional).
+#
+# DC_ACS_LS_SSH_USER: User to login into remote hosts, default: 'root' (optional).
+#
+#
+
+DC_ACS_LS_SERVER_HOST="${DC_ACS_LS_SERVER_HOST:-}"
+DC_ACS_LS_SERVER_USER="${DC_ACS_LS_SERVER_USER:-device}"
+
+DC_ACS_LS_SSH_USER="${DC_ACS_LS_SSH_USER:-root}"
+
+if [ -z "$DC_ACS_LS_SERVER_HOST" ]; then
+	echo "ERROR: DC_ACS_TYPE set to 'ls' but DC_ACS_LS_SERVER_HOST is not set."
+	exit 1
+fi
+
+acs_ip_get_ls() {
+	local id="$1"
+
+	output="$(dc_exec_ssh "$DC_ACS_LS_SERVER_USER@$DC_ACS_LS_SERVER_HOST -- --cmd list --id $id")"
+	ipv4="$(echo "$output" | grep -o -E "IPv4: (.*)" | sed 's/IPv4: //g' | tr -d '\r')"
+
+	echo "$ipv4"
+}
+
+acs_ssh_user_ls() {
+	local id="$1"
+
+	echo "$DC_ACS_LS_SSH_USER"
+}
