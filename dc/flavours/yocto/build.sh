4a04f83351559b0b3b0a9e7b5b32e22cbb7a6c0a
Anibal Limon <anibal@limonsoftware.com>
2025-12-18 15:19:47 -0600

    dc/flavours: yocto add embedded tools for bare metal programming
    Better to rename yocto -> embedded to match both cases of yocto builds
    and embedded programming.
    
    Signed-off-by: Anibal Limon <anibal@limonsoftware.com>

--- dc/flavours/yocto/build.sh
+++ dc/flavours/yocto/build.sh
@@ -8,6 +8,7 @@
 DOCKER_OS_DEVEL_PKGS=" \
 	$DOCKER_OS_DEVEL_PKGS \
 	build-essential \
+	can-utils \
 	chrpath \
 	cpio \
 	debianutils \
@@ -15,17 +16,26 @@ DOCKER_OS_DEVEL_PKGS=" \
 	file \
 	gawk \
 	gcc \
+	gcc-arm-none-eabi \
 	git \
 	iputils-ping \
 	libacl1 \
+	libftdi1 \
+	libhidapi-hidraw0 \
 	liblz4-1 \
+	libusb-1.0-0 \
 	locales \
+	nanopb \
+	protobuf-compiler \
 	python3 \
 	python3-git \
 	python3-jinja2 \
 	python3-pexpect \
 	python3-pip \
-	python3-subunit socat \
+	python3-protobuf \
+	python3-subunit \
+	python3-virtualenv \
+	socat \
 	texinfo \
 	unzip \
 	wget \
