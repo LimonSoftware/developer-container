19f6ed19074753aab97f1bc5461c043283822c55
Anibal Limon <anibal@limonsoftware.com>
2026-03-16 09:27:50 -0600

    dc/services: tailscaled.sh update cmdline
    New tailscale versions uses different cmdline so update it.
    
    Signed-off-by: Anibal Limon <anibal@limonsoftware.com>

--- dc/services/tailscaled.sh
+++ dc/services/tailscaled.sh
@@ -8,4 +8,4 @@ source /etc/default/tailscaled
 TAILSCALED_DIR="$WORKSPACE_RUN_DIR/tailscaled"
 mkdir -p "$TAILSCALED_DIR"
 
-/usr/sbin/tailscaled --state="$TAILSCALED_DIR/tailscaled.state" --socket=/run/tailscale/tailscaled.sock --port=${PORT} $FLAGS
+/usr/sbin/tailscaled -statedir "$TAILSCALED_DIR" -socket "/run/tailscale/tailscaled.sock" -port "${PORT}" $FLAGS
